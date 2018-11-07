07a1470ee8d271a4488ec82b87698137ddef7bff
IyAmGroot <ericfinney1@gmail.com>
2018-11-07 18:07:22 -0500

    NOV 7 Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,26 @@
 var school = {
-  name: "Notre Dame",
-  mascot: "Fightin Irish",
-  logo: "x",
-  "fight-song": "x",
+  name: "NOTRE DAME",
+  mascot: "FIGHTIN IRISH",
+  logo: "assets/images/UND-FI.jpg",
+  fightSong: "x",
 
   playSong: function() {
     console.log("playing song");
   },
   getLogo: function() {
     return this.logo;
+  },
+  getSong: function() {
+    return this.fightSong;
   }
 };
+
+//object.assign(a,b) - combines objects.  create obj with key:values, obj with fn and merge
+//OR
+//create an object and then instantiate it
+//schools[i] = new School()
+
+var schoolsList = [{}];
 
 //Press space bar to begin/reset
 
@@ -29,79 +39,108 @@ var school = {
 /************************Variables*******************************************/
 var mysteryWord = "",
   badGuess = 0,
-  usedLetters = "",
   gameStart = false,
-  displayText = "",
-  tempMascot = "DUKES",
-  tempSchool = "James Madison University";
-
+  winner = "assets/images/winner.gif",
+  loser = "assets/images/loser.jpg";
 /******************************LOGIC*****************************************/
-displayText = document.getElementById("guessMe");
-usedLetters = document.getElementById("letterList");
-displaySchool = document.getElementById("schoolSpan");
-currentDown = document.getElementById("downCount");
 
-document.onkeyup = function() {
-  var tempLetter = "";
-  if (event.code === "Space") {
+var schools = [];
+schools.push(school);
+
+$(document).ready(function() {
+  $("#startBtn").on("click", function() {
     if (!gameStart) {
       newGame();
+    } else {
+      if (confirm("Abandon current game?")) {
+        //Game Breaks here.  Need a Reset fn to empty all the stuff.
+        resetGame();
+        newGame();
+      } else {
+        return;
+      }
+    }
+  });
 
+  $(document).keyup(function(ev) {
+    if (!gameStart) {
       return;
     }
-  }
-  if (!gameStart) {
-    return;
-  }
+    var tempLetter = ev.key;
+    tempLetter = tempLetter.toUpperCase();
 
-  tempLetter = event.key;
-  tempLetter = tempLetter.toUpperCase();
-
-  if (event.keyCode > 64 && event.keyCode < 91) {
-    if (tempMascot.indexOf(tempLetter) < 0) {
-      buildUsedLetterList(tempLetter);
-    } else {
-      setLetter(tempLetter);
+    if (ev.keyCode > 64 && ev.keyCode < 91) {
+      if (school.mascot.indexOf(tempLetter) < 0) {
+        buildUsedLetterList(tempLetter);
+      } else {
+        setLetter(tempLetter);
+      }
     }
-  }
 
-  isGameOver();
-};
+    isGameOver();
+  });
+});
 
 /************************FUNCTIONS*********************************************/
+function resetGame() {
+  $("#schoolSpan").empty();
+  $("#letterList").empty();
+  $("#schoolPic").attr("src", "assets/images/Unknown.png");
+  //school song
+
+  mysteryWord = "";
+  $("#winLose").addClass("hide");
+  badGuess = 0;
+  console.log(mysteryWord);
+  console.log(badGuess);
+}
 function newGame() {
-  displaySchool.innerHTML = tempSchool;
+  //Code goes HERE TO SELECT RANDOM SCHOOL
+  school = schools[Math.floor(Math.random() * schools.length)];
+
+  //Set School Name Here
+  $("#schoolSpan").html(school.name);
+
+  //Populate Underscores
   initializeMysteryWord();
   gameStart = true;
 }
 function isGameOver() {
   if (badGuess > 3) {
+    $("#schoolPic").attr("src", loser);
     alert("you lose");
+    gameStart = false;
   }
-  if (mysteryWord === tempMascot) {
+  if (mysteryWord === school.mascot) {
+    var myLogo = school.getLogo();
+    $("#schoolPic").attr("src", myLogo);
+    $("#winLose").removeClass("hide");
     school.playSong();
+    gameStart = false;
   }
 }
 function buildUsedLetterList(letter) {
   var tempGuess;
-  if (usedLetters.innerHTML.indexOf(letter) >= 0) {
+  var ul2 = $("#letterList");
+  if (ul2.html().indexOf(letter) >= 0) {
     return;
   }
-  if (usedLetters.innerHTML.length === 0) {
-    usedLetters.innerHTML += letter;
+
+  if (ul2.html().length === 0) {
+    ul2.append(letter);
   } else {
-    usedLetters.innerHTML += ", " + letter;
+    ul2.append(", " + letter);
   }
   badGuess++;
   tempGuess = badGuess + 1;
   tempGuess = tempGuess.toString();
-  currentDown.innerHTML = tempGuess;
+  $("#downCount").html(tempGuess);
 }
 
 function setLetter(letter) {
   var tempWord = "";
-  for (var i = 0; i < tempMascot.length; i++) {
-    if (tempMascot.charAt(i) === letter && mysteryWord.charAt(i) === "_") {
+  for (var i = 0; i < school.mascot.length; i++) {
+    if (school.mascot.charAt(i) === letter && mysteryWord.charAt(i) === "_") {
       tempWord += letter;
     } else if (mysteryWord.charAt(i) !== "_") {
       tempWord += mysteryWord.charAt(i);
@@ -111,13 +150,20 @@ function setLetter(letter) {
   }
 
   mysteryWord = tempWord;
-  displayText.innerHTML = mysteryWord;
-  //if displayText == mascot Win, Game Over
+
+  $("#guessMe").html(mysteryWord);
+  //if guessMe == mascot Win, Game Over
 }
 function initializeMysteryWord() {
-  for (var i = 0; i < tempMascot.length; i++) {
-    mysteryWord = mysteryWord + "_";
+  //Sets blanks for letters for school nickname
 
-    displayText.innerHTML = mysteryWord;
+  for (var i = 0; i < school.mascot.length; i++) {
+    if (school.mascot.charAt(i) === " ") {
+      mysteryWord += " ";
+    } else {
+      mysteryWord += "_";
+    }
   }
+
+  $("#guessMe").html(mysteryWord);
 }
